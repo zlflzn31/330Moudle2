b2f383890dd8660755c6b8aa7499df68e8b5f34f
sampointer97 <43008153+sampointer97@users.noreply.github.com>
2018-09-10 14:53:38 -0500

    Update deleteFile.php

--- deleteFile.php
+++ deleteFile.php
@@ -1,1 +1,12 @@
+// use the unlink function in php
+<?php
+$fh = fopen('test.html', 'a');
+fwrite($fh, '<h1>Hello world!</h1>');
+fclose($fh);
 
+unlink('test.html');
+?>
+//this is copied from the php website
+// not sure yet how to load for each
+// may call in a form for each file.
+// cannot test yet
